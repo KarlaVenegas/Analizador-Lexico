dd71f8916c9293395a8e8e88c69fdb5eb085c1be
Jesua Issac Samano Resendiz <isaacresendiz480@gmail.com>
2023-10-05 22:04:23 -0600

    Update practica1.py

--- practica1.py
+++ practica1.py
@@ -297,7 +297,7 @@ def automataPalabrasReserv(linea, cadena, ListaTokens):
 #automata numeros (moverlo aqui)
 
 #automata comentarios (moverlo aqui)
-
+#revisar cuando esta en el estado
 def main():
     ListaTokens=[]
     linea=1
