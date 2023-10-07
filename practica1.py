59bb0bca3029b9e4d2bbf3e6c788d209be89a119
JesusCadenasA <146914245+JCA9@users.noreply.github.com>
2023-10-07 10:11:00 -0600

    Update practica1.py

--- practica1.py
+++ practica1.py
@@ -1,100 +1,6 @@
 import argparse
 ListaTokens =[]
 
-
-
-def automataNumeros (linea,cadenas,ListaTokens)
-    alfa=["1","2","3","4","5","6","7","8","9"]
-    TC=["0",["1","2","3","4","5","6","7","8","9","0"],"15"],
-       ["15",["1","2","3","4","5","6","7","8","9","0"],"15"],
-       ["15",["."],"16"],
-       ["15",["E"],"16"],
-       ["16",["1","2","3","4","5","6","7","8","9","0"],"17"],
-       ["17",["1","2","3","4","5","6","7","8","9","0"],"17"],
-       ["17",["E"],"18"],
-       ["18",["+","-"],"19"],
-       ["18",["1","2","3","4","5","6","7","8","9","0"],"20"],
-       ["18",["1","2","3","4","5","6","7","8","9","0"],"20"],
-       ["20",["1","2","3","4","5","6","7","8","9","0"],"20"]
-    EI="0"
-    EA=EA
-    EF=["15","17","20"]
-    B=True
-    cont=0
-    lex=""
-
-    for caracter in cadena:
-        if caracter == " ":
-            cont = cont + 1
-        else:
-            if caracterin alfa:
-                if caracter == "+" or caracter == "-":
-                    if EA = 19:
-                        lex+lex+caracter
-                        cont=cont+1
-                        for f in TC:
-                            if caracter in f[2] adn EA in f[0]:
-                                TC.append([EA,caracter,f[2]])
-                                EA=f[2]
-                                breal
-                    else:
-                        if!="":
-                            ListaTokens.append("NUMBER")
-                            ListaTokens.append(lex)
-                            num=float(lex)
-                            ListaTokens.append(num)
-                            reconoceUnToken(linea,cadena[cont:lent(cadena)], ListaTokens)
-                            return 0
-                        else:
-                            reconoceUnToken(linea,cadena[cont:lent(cadena)], ListaTokens)
-                            return 0
-                else:
-                    lex=lex+caracter
-                    cont=cont+1
-                    for f in TC:
-                        if caracter in f[1] and EA in f[0]:
-                            TC.append([EA,caracter,f[2]])
-                            EA=f[2]
-                            break
-
-        else: #POR SI NO ESTA EN EL ALFABETO
-            if EA in EI:
-                if lex != "":
-                    ListaTokens.append("NUMBER")
-                    ListaTokens.append(lex)
-                    num=float(lex) 
-                    ListaTokens.append(num)
-                    reconoceUnToken(linea,cadena[cont:len(cadena)],ListaTokens)
-                    return 0
-                else:
-                    reconoceUnToken(linea,cadena[cont:len(cadena)],ListaTokens)
-                    return 0
-            else: #Por si no encuentra ningun numero
-            if cadena [cont:len(cadena)]:
-                reconoceUnToken(linea,cadena[cont:len(cadena)],ListaTokens)
-                return 0
-            else:
-                return 0
-
-    if EA in EF:
-        ListaTokens.append ("NUMBER")
-        ListaTokens.append(lex)
-        num=float(lex)
-        ListaTokens.append(lex)
-        if cadena[cont:len(Cadena)]:
-            reconoceUnToken(linea,cadena[cont:len(cadena)],ListaTokens)
-        else: #CUANDO YA NO HAY CADENAS QUE LEER
-            return 0
-
-    else:
-        print(f"ERROR en linea{linea}")
-        if cadena[cont:len(cadena)]:
-            reconoceUnToken(linea,cadena[cont:len(cadena)],ListaTokens)
-        else:
-            return 0
-
-    #Falta Revisar posible errores                 
-
 def automataCadenas(linea , cadena, ListaTokens):
     EA='0'
     EI = EA
@@ -162,6 +68,7 @@ def automataCadenas(linea , cadena, ListaTokens):
 
 
 #esPalReservada
+
 
 
 def automataPalabrasReserv(linea, cadena, ListaTokens):
@@ -427,10 +334,97 @@ def reconoceUnToken (linea,cadena,ListaTokens):
 
 
 
+def automataNumeros (linea,cadenas,ListaTokens)
+    alfa=["1","2","3","4","5","6","7","8","9"]
+    TC=["0",["1","2","3","4","5","6","7","8","9","0"],"15"],
+       ["15",["1","2","3","4","5","6","7","8","9","0"],"15"],
+       ["15",["."],"16"],
+       ["15",["E"],"16"],
+       ["16",["1","2","3","4","5","6","7","8","9","0"],"17"],
+       ["17",["1","2","3","4","5","6","7","8","9","0"],"17"],
+       ["17",["E"],"18"],
+       ["18",["+","-"],"19"],
+       ["18",["1","2","3","4","5","6","7","8","9","0"],"20"],
+       ["18",["1","2","3","4","5","6","7","8","9","0"],"20"],
+       ["20",["1","2","3","4","5","6","7","8","9","0"],"20"]
+    EI="0"
+    EA=EA
+    EF=["15","17","20"]
+    B=True
+    cont=0
+    lex=""
 
-#automata numeros (moverlo aqui)
+    for caracter in cadena:
+        if caracter == " ":
+            cont = cont + 1
+        else:
+            if caracterin alfa:
+                if caracter == "+" or caracter == "-":
+                    if EA = 19:
+                        lex+lex+caracter
+                        cont=cont+1
+                        for f in TC:
+                            if caracter in f[2] adn EA in f[0]:
+                                TC.append([EA,caracter,f[2]])
+                                EA=f[2]
+                                breal
+                    else:
+                        if!="":
+                            ListaTokens.append("NUMBER")
+                            ListaTokens.append(lex)
+                            num=float(lex)
+                            ListaTokens.append(num)
+                            reconoceUnToken(linea,cadena[cont:lent(cadena)], ListaTokens)
+                            return 0
+                        else:
+                            reconoceUnToken(linea,cadena[cont:lent(cadena)], ListaTokens)
+                            return 0
+                else:
+                    lex=lex+caracter
+                    cont=cont+1
+                    for f in TC:
+                        if caracter in f[1] and EA in f[0]:
+                            TC.append([EA,caracter,f[2]])
+                            EA=f[2]
+                            break
 
+        else: #POR SI NO ESTA EN EL ALFABETO
+            if EA in EI:
+                if lex != "":
+                    ListaTokens.append("NUMBER")
+                    ListaTokens.append(lex)
+                    num=float(lex) 
+                    ListaTokens.append(num)
+                    reconoceUnToken(linea,cadena[cont:len(cadena)],ListaTokens)
+                    return 0
+                else:
+                    reconoceUnToken(linea,cadena[cont:len(cadena)],ListaTokens)
+                    return 0
+            else: #Por si no encuentra ningun numero
+            if cadena [cont:len(cadena)]:
+                reconoceUnToken(linea,cadena[cont:len(cadena)],ListaTokens)
+                return 0
+            else:
+                return 0
 
+    if EA in EF:
+        ListaTokens.append ("NUMBER")
+        ListaTokens.append(lex)
+        num=float(lex)
+        ListaTokens.append(lex)
+        if cadena[cont:len(Cadena)]:
+            reconoceUnToken(linea,cadena[cont:len(cadena)],ListaTokens)
+        else: #CUANDO YA NO HAY CADENAS QUE LEER
+            return 0
+
+    else:
+        print(f"ERROR en linea{linea}")
+        if cadena[cont:len(cadena)]:
+            reconoceUnToken(linea,cadena[cont:len(cadena)],ListaTokens)
+        else:
+            return 0
+
+    #Falta Revisar posible errores                 
 
 
 def automataComentarios(linea, cadena, ListaTokens):
