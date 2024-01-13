bc3642cde875399f0f5348397a6e4aabcaa69de9
JesusCadenasA <146914245+JCA9@users.noreply.github.com>
2024-01-13 16:51:53 -0600

    Update StmtBlock.java

--- ProyectoCompleto/StmtBlock.java
+++ ProyectoCompleto/StmtBlock.java
@@ -9,4 +9,34 @@ public class StmtBlock extends Statement{
     StmtBlock(List<Statement> statements) {
         this.statements = statements;
     }
+    public String toString() {
+        return "StmtBlock['" + statements +  ']';
+    }
+
+    public Object exect(TablaSimbolos t){
+        TablaSimbolos nueva = getTablaHash();
+        t.CopiarA(nueva);
+        if(statements.isEmpty()){
+            throw new RuntimeException("No se colocó un cuerpo a la función");
+        }
+        else{
+
+            for(int i = 0; i<statements.size(); i++){
+                if(statements.get(i) instanceof StmtReturn){
+                    return statements.get(i).exect(nueva);
+
+                }
+                else{
+                    statements.get(i).exect(nueva);
+                }
+
+            }
+            t.AsignaValoresDeOtraTabla(nueva);
+
+                return null;
+        }
+
+
+
+    }
 }
