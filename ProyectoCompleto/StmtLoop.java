879ee16247cdf9f3d4e102f13ac59825be01050b
JesusCadenasA <146914245+JCA9@users.noreply.github.com>
2024-01-14 23:01:30 -0600

    StmtLoop modificada

--- ProyectoCompleto/StmtLoop.java
+++ ProyectoCompleto/StmtLoop.java
@@ -8,4 +8,16 @@ public class StmtLoop extends Statement {
         this.condition = condition;
         this.body = body;
     }
+    public String toString() {
+        return "StmtLoop['" + "condition: " +condition + "body: "+ body + ']';
+    }
+    public Object exect(TablaSimbolos t){
+
+        while ((Boolean) condition.solve(t)){
+            body.exect(t);
+        }
+
+        return null;
+
+    }
 }
