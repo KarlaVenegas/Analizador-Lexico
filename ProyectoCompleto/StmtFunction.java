2821a58ed4ebedce7698f133f3ff892416ce0779
JesusCadenasA <146914245+JCA9@users.noreply.github.com>
2024-01-14 22:52:40 -0600

    También el toString y TblSimbolos  StmtFunction

--- ProyectoCompleto/StmtFunction.java
+++ ProyectoCompleto/StmtFunction.java
@@ -14,4 +14,28 @@ public class StmtFunction extends Statement {
         this.params = params;
         this.body = body;
     }
+        public String toString() {
+        return "StmtFunction['" + name + "', params=" + params + ", body=" + body + ']';
+    }
+    public Object exect(TablaSimbolos t){
+        if(t.existeIdentificador(name.lexema)){
+            throw new RuntimeException("Se define a la misma función más de 1 vez.");
+        }
+        else{
+            //recorro los parámetros para meterlos a la tabla de simbolos
+            for(int i = 0; i< params.size(); i++){
+                if(!this.getTablaHash().existeIdentificador(params.get(i).lexema)){
+                    this.getTablaHash().asignar(params.get(i).lexema, null);
+                }
+                else{
+                    throw new RuntimeException("Parámetros repetidos en la función " + name.lexema);
+                }
+
+            }
+            t.asignar(name.lexema, this);
+        }
+        return null;
+
+    }
 }
+
