4b313d6f38036041a5d9bab2a43bd2690e3ef69c
Jesua Issac Samano Resendiz <isaacresendiz480@gmail.com>
2024-01-14 18:47:18 -0600

    mas para tablas

--- ProyectoCompleto/TablaSimbolos.java
+++ ProyectoCompleto/TablaSimbolos.java
@@ -1,4 +1,5 @@
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class TablaSimbolos {
@@ -50,6 +51,61 @@ public class TablaSimbolos {
 
     void asignar(String identificador, Object valor){
         values.put(identificador, valor);
+    }
+
+    //Recorre una lista y asgina a una TablaSimbolos
+    void recorrerAsigna(List<Object> params){
+        for(Map.Entry<String, Object> entry : values.entrySet()){
+            String clave = entry.getKey();
+            values.put(clave, params.remove(params.size()-1));
+        }
+    }
+
+    //Muestra tablaSimbolos
+    void mostrar(){
+        for (Map.Entry<String, Object> entry : values.entrySet()) {
+            System.out.println("Clave: " + entry.getKey() + ", Valor: " + entry.getValue());
+        }
+    }
+
+    boolean sonValoresIguales() {
+        // Obtener el primer valor del HashMap
+        Object primerValor = null;
+        String aux = null, aux2;
+
+
+
+        for (Object valor : values.values()) {
+            primerValor = valor;
+            if(primerValor instanceof Number){
+                aux = "NUMBER";
+            }
+            else if(primerValor instanceof String){
+                aux= "STRING";
+            }
+            else
+                aux = "a";
+
+            break;
+        }
+
+        // Verificar si todos los valores son iguales al primer valor
+        for (Object valor : values.values()) {
+            if(valor instanceof Number){
+                aux2 = "NUMBER";
+            }
+            else if(valor instanceof String){
+                aux2= "STRING";
+            }
+            else
+                aux2 = "a";
+
+            if (aux!=aux2) {
+                return false; // Se encontró un valor diferente
+            }
+        }
+
+        return true; // Todos los valores son iguales
     }
 
 
