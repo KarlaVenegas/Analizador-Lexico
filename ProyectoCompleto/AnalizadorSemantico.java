36094390881a932092792e19f5584eb18d04ac0e
Jesua Issac Samano Resendiz <isaacresendiz480@gmail.com>
2024-01-14 18:51:52 -0600

    Update AnalizadorSemantico.java

--- ProyectoCompleto/AnalizadorSemantico.java
+++ ProyectoCompleto/AnalizadorSemantico.java
@@ -1,3 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class AnalizadorSemantico {
+    private List<Statement> st;
+    public  AnalizadorSemantico(List<Statement> st){
+        this.st = st;
+    }
+
+    public void proceso(){
+
+        List<Statement> Funciones = new ArrayList<>();
+       // System.out.println(st.get(0).toString());
+        System.out.println(st.toString());
+
+        TablaSimbolos general = new TablaSimbolos();
+
+        for(Statement aux: st){
+            aux.exect(general);
+        }
+    }
     
 }
