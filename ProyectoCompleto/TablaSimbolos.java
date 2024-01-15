c26353a3245f8183dd3f74ca4899cde02dfcd3e1
Jesua Issac Samano Resendiz <isaacresendiz480@gmail.com>
2024-01-14 18:41:56 -0600

    algunos métodos de  tablas de simbolos

--- ProyectoCompleto/TablaSimbolos.java
+++ ProyectoCompleto/TablaSimbolos.java
@@ -16,6 +16,36 @@ public class TablaSimbolos {
         throw new RuntimeException("Variable no definida '" + identificador + "'.");
     }
 
+    void CopiarA(TablaSimbolos nueva){
+        nueva.values.putAll(this.values);
+    }
+
+    void AsignaValoresDeOtraTabla(TablaSimbolos OtraTabla){
+        for(Map.Entry<String, Object> aux: OtraTabla.values.entrySet()){
+            String clave = aux.getKey();
+            Object valor = aux.getValue();
+
+            if(values.containsKey(clave)){
+                values.put(clave, valor);
+            }
+
+        }
+
+    }
+
+    void AsignaStatements(TablaSimbolos OtraTabla){
+        for(Map.Entry<String, Object> aux: OtraTabla.values.entrySet()){
+            String clave = aux.getKey();
+            Object valor = aux.getValue();
+
+            if(valor instanceof StmtFunction){
+                values.put(clave, valor);
+            }
+        }
+    }
+
+
+
     void asignar(String identificador, Object valor){
         values.put(identificador, valor);
     }
